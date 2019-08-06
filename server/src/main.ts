7d7266fa574551196a52fcac55305a1fc5f556be
Rory Mulligan <rorticus@gmail.com>
2019-08-06 19:52:52 -0400

    WIP

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app/app.module';
 import { resolve } from 'path';
 import * as express from 'express';
 
-const gamePath = process.env.STATIC_PATH || resolve(__dirname, '/client');
+const gamePath = process.env.STATIC_PATH || resolve(__dirname, '../../client/build');
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
